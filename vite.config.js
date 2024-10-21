b47f377bf1c41ddcdbbccea3c5355955d28c2adb
AleKoles <kolesnikova.alexandra@gmail.com>
2024-10-21 15:54:15 +0200

    add netlify form and footer

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: './scripts/main.js', 
+      input: "./scripts/main.js",
     },
   },
 });
