b88eb64c71eb069a02f27d9ae20b84ed87df4f4e
Aleksandra Kolesnikova <52916334+AleKoles@users.noreply.github.com>
2024-11-01 16:44:00 +0100

    change vite config and paths

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    outDir: "assets", // Specify the folder where Vite should output files
     rollupOptions: {
       input: "./scripts/main.js",
+      output: {
+        entryFileNames: "main.js", // Specify a fixed name for the main JS file
+      },
     },
   },
 });
