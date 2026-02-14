ce3e35d6b538e9c02e0015bf6ff59adc93825cf4
Sasha <kolesnikova.alexandra@gmail.com>
2026-02-14 14:45:20 +0100

    Fix Vite build for Netlify; ignore build output

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "assets", // Specify the folder where Vite should output files
-    rollupOptions: {
-      input: "./scripts/main.js",
-      output: {
-        entryFileNames: "main.js", // Specify a fixed name for the main JS file
-      },
-    },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
